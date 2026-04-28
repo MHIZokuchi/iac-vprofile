4d2113869bc71d87616347d06a82866f0a528a72
MHIZokuchi <nnekauzoigwe@gmail.com>
2026-04-28 06:45:41 +0100

    jj

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.70"
+      version = ">= 5.95, < 6.0"
     }
     random = {
       source  = "hashicorp/random"
